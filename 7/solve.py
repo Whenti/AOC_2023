f76495431487c78791df3360da94d11bca4e5aa4
Quentin Leveque <qleveque@hotmail.com>
2023-12-07 15:20:23 +0100

    use dec instead of hex

--- 7/solve.py
+++ 7/solve.py
@@ -5,24 +5,25 @@ lines = sys.stdin.readlines()
 labels = ['23456789TJQKA', 'J23456789TQKA']
 
 for part, label in enumerate(labels):
-    map = {i: l for l, i in enumerate(label) }
+    map = {l: i for i, l in enumerate(label) }
     couples = []
 
     for line in lines:
         hand, value = line.split(' ')
-        numbers = [0] * 16
-        for x in hand:
-            numbers[map[x]] += 1
+        numbers = [0] * len(label)
+        cards = [map[card] for card in hand]
+        for card in cards:
+            numbers[card] += 1
 
         if part:
-            joker_number = numbers[0]
-            numbers[0] = 0
+            joker_number = numbers[map['J']]
+            numbers[map['J']] = 0
             max_index = numbers.index(max(numbers))
             numbers[max_index] += joker_number
 
-        hand_hex = f'{max(numbers)}{numbers.count(2)}'
-        hand_hex += ''.join(hex(map[x])[-1] for x in hand)
-        couples.append((int(hand_hex, 16), int(value)))
+        game_power = f'{max(numbers)}{numbers.count(2)}'
+        game_power += ''.join(str(card).zfill(2) for card in cards)
+        couples.append((game_power, int(value)))
 
     couples.sort(key=lambda x: x[0])
     ans = 0
