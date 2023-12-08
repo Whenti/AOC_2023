9a07ca8edae7d0c1d26acab0d735d24a08f7fb58
Quentin Leveque <qleveque@hotmail.com>
2023-12-08 08:53:15 +0100

    use .back() for last char of string

--- 8/solve.cpp
+++ 8/solve.cpp
@@ -7,12 +7,11 @@ int main() {
   readline();
   map<string, pair<string, string>> m;
   vector<string> start_nodes;
-  while(read(from)) {
-    if(from[from.size() - 1] == 'A') start_nodes.push_back(from);
+  while (read(from)) {
+    if (from.back() == 'A') start_nodes.push_back(from);
     readline(to);
     replace(to, " =()", "");
-    replace(to, ",", " ");
-    vector<string> temp = split(to, ' ');
+    vector<string> temp = split(to, ',');
     m[from] = make_pair(temp[0], temp[1]);
   }
   // part 1
@@ -20,21 +19,19 @@ int main() {
   size_t cursor = 0, idx = 0;
   while (current != "ZZZ") {
     current = path[cursor] == 'L' ? m[current].first : m[current].second;
-    if(++cursor >= path.size()) cursor = 0;
+    if (++cursor >= path.size()) cursor = 0;
     idx++;
   }
   print("part 1:", idx);
 
-  //part 2
-  vector<string> currents;
-  for(string const& start_node: start_nodes) currents.push_back(start_node);
+  // part 2
   vector<int> steps;
-  for(string const& start_node: start_nodes) {
+  for (string const &start_node : start_nodes) {
     string current = start_node;
     size_t cursor = 0, idx = 0;
-    while(current[current.size() -1 ] != 'Z') {
+    while (current.back() != 'Z') {
       current = path[cursor] == 'L' ? m[current].first : m[current].second;
-      if(++cursor >= path.size()) cursor = 0;
+      if (++cursor >= path.size()) cursor = 0;
       idx++;
     }
     steps.push_back(idx);
